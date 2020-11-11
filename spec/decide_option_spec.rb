d3b2f8615078770e11a45e76534e738eae652e4b
Kenta Yamamoto <knt.0603.y@gmail.com>
2020-11-11 11:45:04 +0900

    edit decide_option_spec.rb

--- spec/decide_option_spec.rb
+++ spec/decide_option_spec.rb
@@ -2,7 +2,8 @@ RSpec.describe DecideOption do
   it "check decide option" do
     [['test1.org', 'private'],
      ['test2.org', 'teams'],
-     ['test3.org', 'public']
+     ['test3.org', 'public'],
+     ['test4.org', 'private']
     ].each do |src, option|
       p [src, option]
       expect(DecideOption.new("spec/tests/#{src}").decide_option()).to eq option
